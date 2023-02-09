d602a6cf3f2174c01f8481d8e8a9ef6280be0b9a
vscanas <victor@deangraziosi.com>
2023-02-09 14:43:48 -0500

    commit

---
+++ Coderhouse/jsxytranspiling.jsx
@@ -0,0 +1,31 @@
+//Sugar syntax
+//Hacer codigo mas eficiente -
+// i++ = i + i
+
+/*
+Spread operator
+Propiedades dinámicas
+Deep matching
+Asignación en desestructuración 
+*/
+
+/*Polyfills y la retrocompatibilidad
+La idea de un polyfill es rellenar una funcionalidad que necesita
+un navegador si es muy antiguo. 
+
+*/
+/*
+ */
+
+/*Bundling con WEBPACK
+Es un empaquetador de módulos para aplicaciones JavaScript modernas.
+*/
+
+//Transpiling con BABEL
+//Classname se usa con JSX
+<div>
+  <div className="container">
+    <h1>Comission de React</h1>
+  </div>
+  <article></article>
+</div>;
